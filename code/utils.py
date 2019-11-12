3acf28433cf8d4381cdccacdc51f8ae31b411f45
Ofri Kleinfeld <ofri64@gmail.com>
2019-11-12 11:29:45 +0200

    adapted utils file to read data files with respect to current directories stracture

--- code/utils.py
+++ code/utils.py
@@ -1,11 +1,14 @@
 # This file provides code which you may or may not find helpful.
 # Use it if you want, or ignore it.
-import random
+import os
+from collections import Counter
 
 
 def read_data(fname):
     data = []
-    for line in file(fname):
+    absolute_fame = os.path.join(DATA_DIR, fname)
+
+    for line in open(absolute_fame, "r", encoding="utf8"):
         label, text = line.strip().lower().split("\t", 1)
         data.append((label, text))
     return data
@@ -15,10 +18,10 @@ def text_to_bigrams(text):
     return ["%s%s" % (c1, c2) for c1, c2 in zip(text, text[1:])]
 
 
+DATA_DIR = os.path.join(os.path.curdir, "../", "data")
 TRAIN = [(l, text_to_bigrams(t)) for l, t in read_data("train")]
 DEV = [(l, text_to_bigrams(t)) for l, t in read_data("dev")]
 
-from collections import Counter
 fc = Counter()
 for l, feats in TRAIN:
     fc.update(feats)
@@ -30,4 +33,3 @@ vocab = set([x for x, c in fc.most_common(600)])
 L2I = {l: i for i, l in enumerate(list(sorted(set([l for l, t in TRAIN]))))}
 # feature strings (bigrams) to IDs
 F2I = {f: i for i, f in enumerate(list(sorted(vocab)))}
-
