f7527e84006a9578ac9d6615f6de993a01d191cd
Ofri Kleinfeld <ofri64@gmail.com>
2019-11-12 17:52:48 +0200

    implemneted create classifier, forward pass and predict according to given interfaces

--- code/mlpn.py
+++ code/mlpn.py
@@ -1,137 +1,42 @@
 import numpy as np
 
+from train_mlp1 import randomly_initialize_params
+import mlp1
+import loglinear
+
 STUDENT = {'name': 'Ofri Kleinfeld',
          'ID': '302893680'}
-#
-#
-# class NetworkModule(object):
-#     def __init__(self):
-#         self.layer_input = None
-#         self.layer_output = None
-#         self.layer_grad = None
-#
-#     def __call__(self, *args):
-#         raise NotImplementedError("Sub class must implement forward pass")
-#
-#     def backward(self, *args):
-#         raise NotImplementedError("Sub class must implement backward pass")
-#
-#
-# class AbstractNetworkModuleWithParams(NetworkModule):
-#     def __init__(self):
-#         super().__init__()
-#         self.w = None
-#         self.b = None
-#         self.w_grad = None
-#         self.b_grad = None
-#
-#     def init_weights(self, *args):
-#         raise NotImplementedError("Sub class must implement an initialization method for weights")
-#
-#     def xavier_initialization(self, in_dimension, out_dimension):
-#         self.w = np.random.normal(loc=0, scale=in_dimension, size=(out_dimension, in_dimension))
-#         self.b = np.random.normal(loc=0, scale=in_dimension, size=out_dimension)
-#
-#
-# class Linear(AbstractNetworkModuleWithParams):
-#     def __init__(self, in_dimension, out_dimension):
-#         super().__init__()
-#         self.init_weights(in_dimension, out_dimension)
-#
-#     def init_weights(self, in_dimension, out_dimension):
-#         self.xavier_initialization(in_dimension, out_dimension)
-#
-#     def __call__(self, x):
-#         self.layer_input = x
-#         self.layer_output = np.einsum("i,ij->j", x, self.w) + self.b
-#         return self.layer_output
-#
-#     def backward(self, next_layer_grad):
-#         self.layer_grad = np.einsum("j,ij->i", next_layer_grad, self.w)
-#         self.w_grad = np.einsum("j,i->ij", next_layer_grad, self.layer_input)
-#         self.b_grad = next_layer_grad
-#         return self.layer_grad
-#
-#
-# class Relu(NetworkModule):
-#     @staticmethod
-#     def relu_func(x):
-#         return np.maximum(x, 0)
-#
-#     @staticmethod
-#     def relu_derivative(x):
-#         return x > 0
-#
-#     def __call__(self, z):
-#         self.layer_input = z
-#         self.layer_output = self.relu_func(self.layer_input)
-#         return self.layer_output
-#
-#     def backward(self, next_layer_gard):
-#         self.layer_grad = next_layer_gard * self.relu_derivative(self.layer_input)
-#         return self.layer_grad
-#
-#
-# class Tanh(NetworkModule):
-#     @staticmethod
-#     def tanh_func(x):
-#         return np.tanh(x)
-#
-#     @staticmethod
-#     def tanh_derivative(x):
-#         return 1 - np.tanh(x) ** 2
-#
-#     def __call__(self, z):
-#         self.layer_input = z
-#         self.layer_output = self.tanh_func(self.layer_input)
-#         return self.layer_output
-#
-#     def backward(self, next_layer_gard):
-#         self.layer_grad = next_layer_gard * self.tanh_derivative(self.layer_input)
-#         return self.layer_grad
-#
-#
-# class CELoss(NetworkModule):
-#     @staticmethod
-#     def softmax_func(x):
-#         z = x - np.max(x)
-#         exp_z = np.exp(z)
-#
-#         return exp_z / sum(exp_z)
-#
-#     @staticmethod
-#     def cross_entropy_loss(probs, y):
-#         loss = np.sum(-np.log(probs) * y)
-#         return loss
-#
-#     def __init__(self):
-#         super().__init__()
-#         self.label = None
-#         self.loss = None
-#
-#     def __call__(self, logits, label):
-#         self.layer_input = logits
-#         self.layer_output = self.softmax_func(self.layer_input)
-#
-#         y_one_hot = np.zeros(self.layer_input.shape)
-#         y_one_hot[label] = 1
-#
-#         self.label = y_one_hot
-#         self.loss = self.cross_entropy_loss(self.layer_output, self.label)
-#
-#         return self.loss, self.layer_output
-#
-#     def backward(self):
-#         self.layer_grad = self.layer_output - self.label
-#         return self.layer_grad
 
 
 def classifier_output(x, params):
     # YOUR CODE HERE.
-    return probs
+
+    current_param = 0
+    current_input = x
+    hidden_layers = []
+    while current_param < len(params):
+        W = params[current_param]
+        b = params[current_param + 1]
+        z = mlp1.mat_vec_mul(current_input, W) + b
+
+        hidden_layers.append(z)
+        current_input = z
+        # don't do non linearity in last hidden representation before softmax
+        if current_param + 2 < len(params):
+            h = mlp1.tanh(z)
+            hidden_layers.append(h)
+            current_input = h
+
+        current_param += 2
+
+    probs = loglinear.softmax(current_input)
+    return probs, hidden_layers
+
 
 def predict(x, params):
-    return np.argmax(classifier_output(x, params))
+    probs, _ = classifier_output(x, params)
+    return np.argmax(probs)
+
 
 def loss_and_gradients(x, y, params):
     """
@@ -151,7 +56,25 @@ def loss_and_gradients(x, y, params):
     you should not have gW2 and gb2.)
     """
     # YOU CODE HERE
+
+    probs, hidden_layers = classifier_output(x, params)
+    y_one_hot = np.zeros(probs.shape)
+    y_one_hot[y] = 1
+    loss = loglinear.cross_entropy_loss(probs, y)
+
+    # backprop part
+    # gradients with respect to layers inputs
+
+    layer_gradients = []
+    last_hidden_grad = probs - y_one_hot
+    layer_gradients.append(last_hidden_grad)
+    
+    g_h = mat_vec_mul_reverse(g_o, U)
+    g_z = g_h * tanh_derivative(z)
+
+
     return ...
+
 
 def create_classifier(dims):
     """
@@ -174,5 +97,12 @@ def create_classifier(dims):
     second layer, and so on.
     """
     params = []
+
+    params_shapes = [((in_dim, out_dim), out_dim) for in_dim, out_dim in zip(dims[:-1], dims[1:])]
+    for w_shape, b_shape in params_shapes:
+        params.append(np.zeros(*w_shape))
+        params.append(np.zeros(*b_shape))
+
+    params = randomly_initialize_params(params)
     return params
 
