cad6106b5e7c86f5da167cdd4eb270376f0e2a03
Ofri Kleinfeld <ofri64@gmail.com>
2019-12-05 20:20:00 +0200

    implemented prediction straight from model output to enbale loss and accuracy coputation on dev at once

--- hw2/perdictors.py
+++ hw2/perdictors.py
@@ -8,8 +8,12 @@ class BasePredictor(object):
     def __init__(self, mapper: BaseMapper):
         self.mapper = mapper
 
+    def infer_model_outputs(self, model_outputs: torch.tensor):
+        return NotImplementedError("A class deriving from BasePredictor must implement infer_model_outputs method")
+
     def infer_sample(self, model: torch.nn.Module, tokens_indices: torch.tensor):
-        return NotImplementedError("A class deriving from BasePredictor must implement infer_sample method")
+        model_outputs = model(tokens_indices)
+        return self.infer_model_outputs(model_outputs)
 
     def infer_raw_sample(self, model: torch.nn.Module, raw_sample: list):
         sample_tokens = []
@@ -26,12 +30,11 @@ class WindowModelPredictor(BasePredictor):
     def __init__(self, mapper: BaseMapper):
         super().__init__(mapper)
 
-    def infer_sample(self, model: torch.nn.Module, tokens_indices: torch.tensor):
+    def infer_model_outputs(self, model_outputs: torch.tensor):
         predictions = []
-        model_outputs = model(tokens_indices)
         _, labels_tokens = torch.max(model_outputs, dim=1)
 
-        for i in range(len(tokens_indices)):  # every sample in case of batch (even batch of size 1)
+        for i in range(len(model_outputs)):  # every sample in case of batch (even batch of size 1)
             current_prediction = labels_tokens[i].cpu().numpy()
             predicted_label = self.mapper.get_label_idx(current_prediction)
             predictions.append(predicted_label)
