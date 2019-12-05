dc15e683d0b3323f738245a1550b45ea02b55f1c
Ofri Kleinfeld <ofri64@gmail.com>
2019-12-05 19:30:16 +0200

    initial implementation of a predictor

--- hw2/perdictors.py
+++ hw2/perdictors.py
@@ -1,25 +1,39 @@
 import torch
 from torch.utils import data
-from mappers import TokenMapper
+from mappers import BaseMapper
 
 
 class BasePredictor(object):
 
-    def __init__(self, mapper: TokenMapper):
+    def __init__(self, mapper: BaseMapper):
         self.mapper = mapper
 
-    def infer_sample(self, model: torch.nn.Module, tokenized_text: torch.tensor):
+    def infer_sample(self, model: torch.nn.Module, tokens_indices: torch.tensor):
         return NotImplementedError("A class deriving from BasePredictor must implement infer_sample method")
 
     def infer_raw_sample(self, model: torch.nn.Module, raw_sample: list):
-        return NotImplementedError("A class deriving from BasePredictor must implement infer_raw_sample method")
+        sample_tokens = []
+        for sample in raw_sample:
+            token_indices = [self.mapper.get_token_idx(token) for token in sample]
+            sample_tokens.append(token_indices)
+
+        sample_tokens = torch.tensor(sample_tokens)
+        return self.infer_sample(model, sample_tokens)
+
 
 class WindowModelPredictor(BasePredictor):
 
-    def __init__(self, mapper: TokenMapper):
+    def __init__(self, mapper: BaseMapper):
         super().__init__(mapper)
 
-    def infer_sample(self, model: torch.nn.Module, tokenized_text: torch.tensor):
-        idx_to_label = self.mapper.idx_to_label
+    def infer_sample(self, model: torch.nn.Module, tokens_indices: torch.tensor):
         predictions = []
+        model_outputs = model(tokens_indices)
+        _, labels_tokens = torch.max(model_outputs, dim=1)
 
+        for i in range(len(tokens_indices)):  # every sample in case of batch (even batch of size 1)
+            current_prediction = labels_tokens[i].cpu().numpy()
+            predicted_label = self.mapper.get_label_idx(current_prediction)
+            predictions.append(predicted_label)
+
+        return predictions
