7abe79331a98975b24c7945c1b910f27ab0b3575
Ofri Kleinfeld <ofri64@gmail.com>
2019-12-06 11:07:47 +0200

    fixed tagger1 script for checking th checking this branch

--- hw2/tagger1.py
+++ hw2/tagger1.py
@@ -1,14 +1,15 @@
 from mappers import TokenMapperUnkCategory
-from datasets import WindowDatasetUnkCategories
+from datasets import WindowDataset
 
 if __name__ == '__main__':
     train_path = "pos/train"
     test_path = "pos/test"
     mapper = TokenMapperUnkCategory(min_frequency=5, split_char=" ")
     mapper.create_mapping(train_path)
-    dataset = WindowDatasetUnkCategories(test_path, mapper)
+    dataset = WindowDataset(test_path, mapper)
 
     print(len(dataset))
-    sample, label = dataset[3]
-    print(sample)
-    print(label)
+    for i in range(len(dataset)):
+        sample, label = dataset[i]
+        print(sample)
+        print(label)
