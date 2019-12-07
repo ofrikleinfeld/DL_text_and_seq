fixed bug with stacking word prefix and suffix into 1 tensor and unpacking it during forward computation

hw2/datasets.py
@@ -157,7 +157,7 @@ class WindowWithSubWordsDataset(WindowDataset):
         sample_indices = torch.tensor(sample_indices)
         prefixes_indices = torch.tensor(prefixes_indices)
         suffixes_indices = torch.tensor(suffixes_indices)
-        x = (sample_indices, prefixes_indices, suffixes_indices)
+        x = torch.stack([sample_indices, prefixes_indices, suffixes_indices])
 
         # verify if it a train/dev dataset of a test dataset
         if len(self.labels) > 0:  # we have labels

hw2/models.py
@@ -139,7 +139,9 @@ class WindowModelWithSubWords(ModelWithPreTrainedEmbeddings):
         self.output = nn.Linear(in_features=hidden_dim, out_features=labels_dim)
 
     def forward(self, x: list) -> torch.tensor:
-        words_tokens, prefix_tokens, suffix_tokens = x
+        words_tokens = x[:, 0, :]
+        prefix_tokens = x[:, 1, :]
+        suffix_tokens = x[:, 2, :]
         word_embeddings = self.embedding(words_tokens)
         prefix_embeddings = self.prefix_embedding(prefix_tokens)
         suffix_embeddings = self.suffix_embedding(suffix_tokens)