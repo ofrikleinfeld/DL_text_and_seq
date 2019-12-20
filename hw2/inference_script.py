36188e35edd1e5e56badd9aa94fdcebd85565cee
Ofri Kleinfeld <ofri64@gmail.com>
2019-12-20 23:35:44 +0200

    refactor inference script. adds support for LSTM models with padding

--- hw2/inference_script.py
+++ hw2/inference_script.py
@@ -1,12 +1,14 @@
 import torch
 from torch.utils import data
+
 from factory_classes import ModelsFactory, MappersFactory, ConfigsFactory, PredictorsFactory, DatasetsFactory
 from models import BaseModel
-from mappers import BaseMapper
-from configs import BaseConfig
+from mappers import BaseMapper, BaseMapperWithPadding
+from configs import BaseConfig, ModelConfig
+from datasets import BiLSTMDataset
 
 
-def load_trained_model(path_to_pth_file: str):
+def load_trained_model(path_to_pth_file: str, model_type: str):
     checkpoint_data = torch.load(path_to_pth_file)
 
     # Factories
@@ -23,20 +25,18 @@ def load_trained_model(path_to_pth_file: str):
     model_config_params = config_data["state"]
 
     mapper_data = checkpoint_data["mapper"]
-    mapper_name = mapper_data["name"]
     mapper_state = mapper_data["state"]
 
     # load a config
-    model_name_case_insensitive = model_name.lower()
-    model_config: BaseConfig = configs_factory(model_name_case_insensitive)
+    model_config: ModelConfig = configs_factory(model_type)
     model_config.from_dict(model_config_params)
 
     # create a mapper
-    trained_mapper: BaseMapper = mappers_factory.get_from_mapper_name(mapper_name)
+    trained_mapper: BaseMapper = mappers_factory(BaseConfig(), model_type)
     trained_mapper.deserialize(mapper_state)
 
     # create a model
-    trained_model: BaseModel = models_factory.get_from_model_name(model_name, model_config, trained_mapper)
+    trained_model: BaseModel = models_factory(model_name, model_config, trained_mapper, model_type)
     trained_model.load_state_dict(model_state)
 
     return trained_model, model_name
@@ -59,7 +59,7 @@ def save_predictions_to_file(test_path: str, predictions: list, save_model_path:
                     index += 1  # don't forge to move to next prediction
 
 
-def inference(test_path: str, inference_config_path: str, saved_model_path: str, save_predictions_path: str) -> None:
+def inference(test_path: str, inference_config_path: str, saved_model_path: str, save_predictions_path: str, model_type: str) -> None:
     # initiate factory object
     config_factory = ConfigsFactory()
     predictors_factory = PredictorsFactory()
@@ -67,23 +67,15 @@ def inference(test_path: str, inference_config_path: str, saved_model_path: str,
 
     # load trained model class and initiate a predictor
     inference_config = config_factory("inference").from_json_file(inference_config_path)
-    model, model_name = load_trained_model(saved_model_path)
+    model, _ = load_trained_model(saved_model_path, model_type)
     model: BaseModel
     mapper = model.mapper
-    model_type = inference_config["model_type"]
     predictor = predictors_factory(inference_config, mapper, model_type)
 
     # check if model is a model with sub word units
-    if "window" in model_type:
-        if "SubWords" in model_name:
-            dataset_name = "WindowWithSubWordsDataset"
-        else:
-            dataset_name = "WindowDataset"
-    else:
-        dataset_name = "RegularLanguageDataset"
 
     # create dataset object and preform inference
-    test_dataset = dataset_factory.get_from_dataset_name(dataset_name, test_path, mapper)
+    test_dataset = dataset_factory(BaseConfig(), test_path, mapper, model_type)
     test_config_dict = {"batch_size": inference_config["batch_size"], "num_workers": inference_config["num_workers"]}
     test_loader = data.DataLoader(test_dataset, **test_config_dict)
 
@@ -92,18 +84,49 @@ def inference(test_path: str, inference_config_path: str, saved_model_path: str,
     model.eval()
     predictions = []
 
-    # run the model in batches to create the predictions
-    with torch.no_grad():
+    # if it an LSTM model we must make sure that the sequence length is the maximum sequence length in the data
+    # and to use mask tensor to make sure we only predict for real tokens
+    if "lstm" in model_type:
+        test_dataset: BiLSTMDataset
+        mapper: BaseMapperWithPadding
+        padding_index = mapper.get_padding_index()
+        max_sequence_length = test_dataset.get_dataset_max_sequence_length()
+        test_dataset.sequence_length = max_sequence_length
 
-        for batch_idx, sample in enumerate(test_loader):
-            x, _ = sample
-            x = x.to(device)
-            outputs = model(x)
-            batch_predictions = predictor.infer_model_outputs(outputs)
+        # run the model in batches to create the predictions
+        with torch.no_grad():
 
-            for prediction in batch_predictions:
-                predicted_label = mapper.get_label_from_idx(prediction)
-                predictions.append(predicted_label)
+            for batch_idx, sample in enumerate(test_loader):
+                x, _ = sample
+                x = x.to(device)
+                outputs = model(x)
+
+                real_tokens_mask: torch.tensor
+                if len(x.size()) == 3:
+                    real_tokens_mask = (x[:, 0, :] != padding_index).flatten()
+                else:
+                    real_tokens_mask = (x != padding_index).flatten()
+
+                batch_predictions: torch.tensor = predictor.infer_model_outputs(outputs)
+                batch_predictions = batch_predictions.flatten()
+
+                for i in range(len(batch_predictions)):
+                    if real_tokens_mask[i]:
+                        predicted_label = mapper.get_label_from_idx(batch_predictions[i].item())
+                        predictions.append(predicted_label)
+    else:
+        # run the model in batches to create the predictions
+        with torch.no_grad():
+
+            for batch_idx, sample in enumerate(test_loader):
+                x, _ = sample
+                x = x.to(device)
+                outputs = model(x)
+                batch_predictions = predictor.infer_model_outputs(outputs)
+
+                for prediction in batch_predictions:
+                    predicted_label = mapper.get_label_from_idx(prediction)
+                    predictions.append(predicted_label)
 
     save_predictions_to_file(test_path, predictions, save_predictions_path)
 
