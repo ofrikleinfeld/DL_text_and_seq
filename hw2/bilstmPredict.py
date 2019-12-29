2ffa575a837ff0f16e5451e94512318551492c6f
Ofri Kleinfeld <ofri64@gmail.com>
2019-12-29 18:54:49 +0200

    rearanged the predict and train argument parsers for part 3

--- hw2/bilstmPredict.py
+++ hw2/bilstmPredict.py
@@ -6,28 +6,24 @@ from inference_script import inference
 if __name__ == '__main__':
 
     # create the parser
-    parser = argparse.ArgumentParser(description='train bilstm model')
+    parser = argparse.ArgumentParser(description='predict using a trained bilstm model')
 
-    parser.add_argument("repr", type=str ,choices=['a', 'b', 'c', 'd'], help='which type of word embeddings representation to use')
-    parser.add_argument("model_path", type=str, help="a path to training file")
-    parser.add_argument("test_path", type=str, help="a path to test file")
-    parser.add_argument("--inference_config_path", required=False, type=str, help="a path to inference config file", default="inference_config.json")
-    parser.add_argument("--save_output_path", required=False, type=str, help="a path to save the inference on the test set")
-    parser.add_argument('--pos_or_ner', type=str, required=False,
-                        help="whether to calculate accuracy the ner or normal way", default="pos")
-    model_type_convertor = {'a' : "lstm", 'c' : "lstm_sub_words",
-                        'b' : "lstm_char_embeddings", 'd': "lstm_char_word_embeddings"}
+    parser.add_argument("repr", type=str, choices=['a', 'b', 'c', 'd'], help='which type of word embeddings representation to use')
+    parser.add_argument("model_path", type=str, help="a path to a trained model file")
+    parser.add_argument("test_path", type=str, help="a path to a test file")
+    parser.add_argument('model_type', type=str, choices=["pos", "ner"], help="which model to run POS or NER")
+    parser.add_argument("inference_config_path", type=str, help="a path to inference config file")
+    parser.add_argument("save_output_path", type=str, help="a path to save the results of the inference on the test set")
+
+    model_type_converter = {'a': "lstm", 'c': "lstm_sub_words", 'b': "lstm_char_embeddings", 'd': "lstm_char_word_embeddings"}
+
     # parse the args
     args = parser.parse_args(sys.argv[1:])
-    repr = args.repr
+    repr_ = args.repr
     model_path = args.model_path
     test_path = args.test_path
-    pos_or_ner = args.pos_or_ner
-    model_type = model_type_convertor[repr] + '_' + pos_or_ner
+    model_type = args.model_type
+    model_name = model_type_converter[repr_] + '_' + model_type
     inference_config_path = args.inference_config_path
-    save_output_path = None
-    if not args.save_output_path:
-        save_output_path = save_output_path(model_type + '_' + pos_or_ner)
-    else:
-        save_output_path = args.save_output_path
-    inference(test_path, inference_config_path, model_path, save_output_path, model_type)
+    save_output_path = args.save_output_path
+    inference(test_path, inference_config_path, model_path, save_output_path, model_name)
--- hw2/bilstmTrain.py
+++ hw2/bilstmTrain.py
@@ -8,30 +8,27 @@ if __name__ == '__main__':
     # create the parser
     parser = argparse.ArgumentParser(description='train bilstm model')
 
-    parser.add_argument("repr", type=str ,choices=['a', 'b', 'c', 'd'], help='which type of word embeddings representation to use')
+    parser.add_argument("repr", type=str, choices=['a', 'b', 'c', 'd'], help='which type of word embeddings representation to use')
     parser.add_argument("train_path", type=str, help="a path to training file")
-    parser.add_argument("model_path", type=str, help="a path to test file")
-    parser.add_argument("model_config_path", type=str,
-                                 help="path to a json file containing model hyper parameters")
-    parser.add_argument("training_config_path", type=str,
-                                 help="path to a json file containing hyper parameters for training procedure")
+    parser.add_argument("model_path", type=str, help="a path to save the trained model")
+    parser.add_argument("model_config_path", type=str, help="path to a json file containing model hyper parameters")
+    parser.add_argument("training_config_path", type=str, help="path to a json file containing hyper parameters for training procedure")
+    parser.add_argument('model_type', type=str, choices=["pos", "ner"], help="which model to run POS or NER")
     parser.add_argument("--dev_path", type=str, required=False,  help="a path to dev file")
-    parser.add_argument('--pos_or_ner', type=str, required=False, help="whether to calculate accuracy the ner or normal way", default="pos")
 
-    model_type_convertor = {'a' : "lstm", 'c' : "lstm_sub_words",
-                        'b' : "lstm_char_embeddings", 'd': "lstm_char_word_embeddings"}
+    model_type_converter = {'a': "lstm", 'c': "lstm_sub_words", 'b': "lstm_char_embeddings", 'd': "lstm_char_word_embeddings"}
     # parse the args
     args = parser.parse_args(sys.argv[1:])
-    repr = args.repr
+    repr_ = args.repr
     train_path = args.train_path
     run_name = args.model_path
     if args.dev_path:
         test_path = args.dev_path
     else:
         test_path = args.train_path
-    pos_or_ner = args.pos_or_ner
+    model_type = args.model_type
     model_config_path = args.model_config_path
     training_config_path = args.training_config_path
-    model_type = model_type_convertor[repr] + '_' + pos_or_ner
+    model_name = model_type_converter[repr_] + '_' + model_type
 
-    train(run_name, model_type, train_path, test_path, model_config_path, training_config_path)
+    train(run_name, model_name, train_path, test_path, model_config_path, training_config_path)
