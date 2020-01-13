2adfacdd4aff2b80dbada81545d28f618c5c1bbb
Ofri Kleinfeld <ofri64@gmail.com>
2020-01-13 17:03:39 +0200

    fixing bug in mapper

--- SNLI/snli_mappers.py
+++ SNLI/snli_mappers.py
@@ -2,13 +2,15 @@ import hashlib
 from typing import Dict
 from collections import OrderedDict
 
-from pos_and_ner.mappers import UNK, WORD_PAD, BaseMapperWithPadding
+from pos_and_ner.mappers import BaseMapperWithPadding, TokenMapper
+
+UNK = "unkkkkkkkkkkk"
+WORD_PAD = "paddddddddddddd"
 
 
-class SNLIMapperWithGloveIndices(BaseMapperWithPadding):
+class SNLIMapperWithGloveIndices(BaseMapperWithPadding, TokenMapper):
 
-    def __init__(self, glove_words_path: str, split_char: str = "\t"):
-        min_frequency = 0  # we want to take every word from the raining set, because we use fixed, pre trained, embeddings
+    def __init__(self, min_frequency: int = 5, split_char: str = "\t", glove_words_path: str = ""):
         super().__init__(min_frequency=min_frequency, split_char=split_char)
         self.unknown_label_symbol = "-"
         self.word_to_glove_idx = {}
@@ -17,14 +19,16 @@ class SNLIMapperWithGloveIndices(BaseMapperWithPadding):
     def serialize(self) -> dict:
         serialization = super().serialize()
         serialization["word_to_glove_idx"] = self.word_to_glove_idx
+        serialization["unknown_label_symbol"] = self.unknown_label_symbol
         return serialization
 
     def deserialize(self, serialized_mapper: dict) -> None:
         super().deserialize(serialized_mapper)
         self.word_to_glove_idx = serialized_mapper["word_to_glove_idx"]
+        self.unknown_label_symbol = serialized_mapper["unknown_label_symbol"]
 
     def create_mapping(self, filepath: str) -> None:
-        words = OrderedDict()
+        words_frequencies = OrderedDict()
         labels = OrderedDict()
         glove_words_indices = self._load_glove_words_indices()
 
@@ -37,42 +41,46 @@ class SNLIMapperWithGloveIndices(BaseMapperWithPadding):
                     header_line = False
                     continue
 
-            line_tokens = line[:-1].split(self.split_char)  # remove end of line
-            label = line_tokens[0]
-            # skip samples with unknown label - i.e "-" label
-            if label != self.unknown_label_symbol:
+                line_tokens = line[:-1].split(self.split_char)  # remove end of line
+                label = line_tokens[0]
+                # skip samples with unknown label - i.e "-" label
+                if label != self.unknown_label_symbol:
 
-                sentence_1 = [word.lower() for word in line_tokens[5].split()]
-                sentence_2 = [word.lower() for word in line_tokens[6].split()]
+                    sentence_1 = [word.lower() for word in line_tokens[5].split()]
+                    sentence_2 = [word.lower() for word in line_tokens[6].split()]
 
-                labels[label] = 0
-                sentences_words = sentence_1 + sentence_2
-                for word in sentences_words:
-                    if word not in words:
-                        words[word] = 0
+                    labels[label] = 0
+                    sentences_words = sentence_1 + sentence_2
+                    for word in sentences_words:
+                        words_frequencies[word] = words_frequencies.get(word, 0) + 1
 
-        # only take into account words the appear in training set
+        # only take into account words the appear in training set enough times
         # and has glove pre trained vector
         # all other words will be treated as OOV
+
+        # remove non frequent
+        words = self._remove_non_frequent(words_frequencies)
 
         # first init unknown tokens and padding token
         self._init_unknown_tokens()
         self._init_padding_idx()
 
         # start index will be different if index 0 marked already as padding
-        word_start_index = len(self.token_to_idx)
-        label_start_index = len(self.label_to_idx)
+        word_index = len(self.token_to_idx)
+        label_index = len(self.label_to_idx)
 
-        for index, word in enumerate(words.keys(), word_start_index):
+        for word in words.keys():
             # only if word has pre trained glove vector
             if word in glove_words_indices:
-                self.token_to_idx[word] = index
-                self.idx_to_token[index] = word
+                self.token_to_idx[word] = word_index
+                self.idx_to_token[word_index] = word
                 self.word_to_glove_idx[word] = glove_words_indices[word]
+                word_index += 1
 
-        for index, label in enumerate(labels.keys(), label_start_index):
-            self.label_to_idx[label] = index
-            self.idx_to_label[index] = label
+        for label in labels.keys():
+            self.label_to_idx[label] = label_index
+            self.idx_to_label[label_index] = label
+            label_index += 1
 
     def _load_glove_words_indices(self) -> Dict[str, int]:
         pre_trained_words_indices = {}
@@ -90,7 +98,7 @@ class SNLIMapperWithGloveIndices(BaseMapperWithPadding):
         so we need 10 different "unknown" tokens
         """
         unk_template = UNK + "_{idx}"
-        unk_tokens = {unk_template.format(i): i for i in range(100)}
+        unk_tokens = {unk_template.format(idx=i): i for i in range(100)}
         self.token_to_idx.update(unk_tokens)
         self.idx_to_token = {value: key for key, value in self.token_to_idx.items()}
 
@@ -108,9 +116,11 @@ class SNLIMapperWithGloveIndices(BaseMapperWithPadding):
             hex_dig = hash_object.hexdigest()
             bucket = int(hex_dig, 16) % 100
             unknown_token = f"{UNK}_{bucket}"
-            raw_token = unknown_token
+            token = unknown_token
+        else:
+            token = lower_raw_token
 
-        return self.token_to_idx[raw_token]
+        return self.token_to_idx[token]
 
     def get_label_idx(self, raw_label: str) -> int:
         return self.label_to_idx[raw_label]
